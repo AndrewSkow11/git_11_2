cda5d7fcb0a8438906622655df32cd9cff3aa893
Andrew Skow <skovorodnikov11@gmail.com>
2023-10-04 09:55:38 +0300

    added str_func.py file

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def big_letters(str):
+    """Другой док-стринг"""
     return str.upper()
 
 print(big_letters("little lettrs"))
+
+def capital_strin(str):
+    """Капитализация принятой строки"""
+    return str.capitalize()
+
+
+print(capital_strin("little lettrs"))
